00c16c32f5e1253a672bc4b0e25635a46c501e42
miguelpantoja89 <mgbastechnology@gmail.com>
2020-08-02 19:24:59 +0200

    Adding more basic commands

---
+++ fundamentals/Commands.R
@@ -0,0 +1,17 @@
+ #### Comandos utilies
+### Ctrl+L para limpiar la consola
+
+#Instalar paquetes
+##install.packages("plotly")
+### Para llamar al paquete 
+print("hello")
+##library(plot)
+help(print)
+x <- 9.45
+y <- (4*x) - log(x) + cos(2*pi*x)
+
+#Eliminar una variable 
+rm(x)
+
+#Eliminar contenido del WKspace
+rm(list = ls())
